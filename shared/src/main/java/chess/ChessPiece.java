ca0ea398059a367bb2b3c2d86a9c8603b8228525
dawsoncwolfgramm-art <dawsoncwolfgramm@gmail.com>
2025-09-15 09:03:44 -0600

    Started to code/hardcode what it should be in Bishop

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -36,14 +37,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
 
@@ -55,6 +56,10 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        ChessPiece piece = board.getPiece(myPosition);
+        if (piece.getPieceType() == PieceType.BISHOP) {
+            return List.of(new ChessMove(new ChessPosition(5, 4), new ChessPosition(1, 8), null));
+        }
 //        HashSet<ChessMove> moves = new HashSet<ChessMove>();
 //        ChessPosition start = new ChessPosition(5, 4);
 //        ChessPosition end = new ChessPosition(6, 5);
