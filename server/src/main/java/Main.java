625033bd4dfaa3901770e92a921efd698a160ff8
dawsoncwolfgramm-art <dawsoncwolfgramm@gmail.com>
2025-11-03 14:33:40 -0700

    changed the main and UserServiceTest.java

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Server server = new Server();
         server.run(8080);
-
         System.out.println("♕ 240 Chess Server");
     }
 }
--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -6,7 +6,6 @@ import datamodel.AuthData;
 import datamodel.GameData;
 import datamodel.JoinGameRequest;
 import datamodel.UserData;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
