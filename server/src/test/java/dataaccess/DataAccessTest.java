760a0b2c511da551df69ed295788cff0f857bf2e
dawsoncwolfgramm-art <dawsoncwolfgramm@gmail.com>
2025-11-03 17:46:46 -0700

    added failed tests

--- server/src/test/java/dataaccess/DataAccessTest.java
+++ server/src/test/java/dataaccess/DataAccessTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.*;
+import service.BadRequestException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,7 +28,7 @@ class DataAccessTest {
 
     @Test
     void createUser() throws Exception {
-        var user = new UserData("dawson", "grousehouse", "daws@gmail.com");
+        var user = new UserData("dawson", "asdfghjkl;", "daws@gmail.com");
         DataAccess da = new MySqlDataAccess();
         da.createUser(user);
         var optUserData = da.getUser(user.username());
@@ -37,13 +38,41 @@ class DataAccessTest {
     }
 
     @Test
+    void createUserFailDuplicateUsername() throws Exception {
+        var u1 = new UserData("walker", "asdfghjkl", "a@a.com");
+        var u2 = new UserData("wyatt", "asdfghjkl", "b@b.com"); // same username (PK)
+        DataAccess da = new MySqlDataAccess();
+        da.clear();
+
+        da.createUser(u1);
+        assertThrows(DataAccessException.class, () -> da.createUser(u2));
+    }
+
+    @Test
+    void createUserFailNulls() throws Exception {
+        var bad = new UserData("dawson", null, null);
+        DataAccess da = new MySqlDataAccess();
+        da.clear();
+
+        assertThrows(DataAccessException.class, () -> da.createUser(bad));
+    }
+
+    @Test
     void getUser() throws Exception {
-        var user = new UserData("dawson", "grousehouse", "daws@gmail.com");
+        var user = new UserData("dawson", "asdfghjkl", "daws@gmail.com");
         DataAccess da = new MySqlDataAccess();
         da.createUser(user);
         var optUserData = da.getUser(user.username());
         UserData userData = optUserData.get();
         assertEquals(userData.username(), user.username());
+    }
+
+    @Test
+    void getUserFail() throws Exception {
+        DataAccess da = new MySqlDataAccess();
+        da.clear();
+        var opt = da.getUser("dawson");
+        assertTrue(opt.isEmpty());
     }
 
     @Test
@@ -59,6 +88,27 @@ class DataAccessTest {
     }
 
     @Test
+    void addAuthFailDuplicateToken() throws Exception {
+        var user = new UserData("dawson", "p12340987123", "a@a.com");
+        var a1 = new AuthData("walker", "12341234");
+        var a2 = new AuthData("wyatt", "12341234");
+        DataAccess da = new MySqlDataAccess();
+        da.clear();
+        da.createUser(user);
+        da.addAuth(a1);
+        assertThrows(DataAccessException.class, () -> da.addAuth(a2));
+    }
+
+    @Test
+    void getAuthFailNotFound() throws Exception {
+        DataAccess da = new MySqlDataAccess();
+        da.clear();
+        var opt = da.getAuth("missing");
+        assertTrue(opt.isEmpty());
+    }
+
+
+    @Test
     void deleteAuth() throws Exception {
         AuthData auth = new AuthData("dawson", "12345678");
         AuthData authTwo = new AuthData("lauren", "87654321");
@@ -71,6 +121,14 @@ class DataAccessTest {
         assertEquals(Optional.empty(), optAuthData);
         var optAuthDataTwo = da.getAuth((authTwo.authToken()));
         assertTrue(optAuthDataTwo.isPresent());
+    }
+
+    @Test
+    void deleteAuthFail() throws Exception {
+        DataAccess da = new MySqlDataAccess();
+        da.clear();
+        da.deleteAuth("doesn't-exist");
+        assertTrue(da.getAuth("doesn't-exist").isEmpty());
     }
 
     @Test
@@ -89,6 +147,22 @@ class DataAccessTest {
         da.clear();
         da.addGame(game);
         da.getGame(1);
+    }
+
+    @Test
+    void addGameFail() throws Exception {
+        var badGame = new GameData(0, null, null, null, null);
+        DataAccess da = new MySqlDataAccess();
+        da.clear();
+        assertThrows(DataAccessException.class, () -> da.addGame(badGame));
+    }
+
+    @Test
+    void getGameFail() throws Exception {
+        DataAccess da = new MySqlDataAccess();
+        da.clear();
+        var opt = da.getGame(1234);
+        assertTrue(opt.isEmpty());
     }
 
     @Test
