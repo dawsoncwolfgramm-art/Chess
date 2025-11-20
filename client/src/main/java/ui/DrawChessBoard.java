eb4cc67ea1f647bfc0dcf0ff6f349327d6dd4c96
dawsoncwolfgramm-art <dawsoncwolfgramm@gmail.com>
2025-11-19 17:32:58 -0700

    decomposing drawChessBoard with simplifying setting the opposite colors

--- client/src/main/java/ui/DrawChessBoard.java
+++ client/src/main/java/ui/DrawChessBoard.java
@@ -3,20 +3,17 @@ package ui;
 import chess.ChessBoard;
 import chess.ChessGame;
 import chess.ChessPiece;
-
 import ui.EscapeSequences.*;
 
 import static ui.EscapeSequences.*;
 
 
 public class DrawChessBoard {
-
     private final String color;
 
     public DrawChessBoard(String color) {
         this.color = color;
     }
-
 
     public void printChessBoard(ChessBoard board) {
         var board1 = board.getBoard();
@@ -39,70 +36,7 @@ public class DrawChessBoard {
                 ChessPiece piece = board1[row][col];
                 boolean lightSquare = (row + col) % 2 == 0;
                 colorBoard.append(lightSquare ? SET_BG_COLOR_LIGHT_GREY : SET_BG_COLOR_DARK_GREEN);
-                String piece1 = EMPTY;
-                if (piece != null) {
-                    if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
-                        switch (piece.getPieceType()) {
-                            case BISHOP: {
-                                piece1 = WHITE_BISHOP;
-                                break;
-                            }
-                            case PAWN: {
-                                piece1 = WHITE_PAWN;
-                                break;
-                            }
-                            case KING: {
-                                piece1 = WHITE_KING;
-                                break;
-                            }
-                            case QUEEN: {
-                                piece1 = WHITE_QUEEN;
-                                break;
-                            }
-                            case ROOK: {
-                                piece1 = WHITE_ROOK;
-                                break;
-                            }
-                            case KNIGHT: {
-                                piece1 = WHITE_KNIGHT;
-                            }
-                        }
-                        colorBoard.append(piece1);
-                    }
-                    if (piece.getTeamColor() == ChessGame.TeamColor.BLACK) {
-                        switch (piece.getPieceType()) {
-                            case BISHOP: {
-                                piece1 = BLACK_BISHOP;
-                                break;
-                            }
-                            case PAWN: {
-                                piece1 = BLACK_PAWN;
-                                break;
-                            }
-                            case KING: {
-                                piece1 = BLACK_KING;
-                                break;
-                            }
-                            case QUEEN: {
-                                piece1 = BLACK_QUEEN;
-                                break;
-                            }
-                            case ROOK: {
-                                piece1 = BLACK_ROOK;
-                                break;
-                            }
-                            case KNIGHT: {
-                                piece1 = BLACK_KNIGHT;
-                                break;
-                            }
-                        }
-                        colorBoard.append(piece1);
-                    }
-                } else {
-                    piece1 = EMPTY;
-                    colorBoard.append(piece1);
-                }
-
+                colorBoard.append(symbolForChess(piece));
             }
             white = (!white);
             colorBoard.append(" ").append(rank).append("\n");
@@ -124,5 +58,22 @@ public class DrawChessBoard {
             }
         }
         return flipped;
+    }
+
+    private String symbolForChess(ChessPiece piece) {
+        if (piece == null) {
+            return EMPTY;
+        }
+
+        boolean isWhite = piece.getTeamColor() == ChessGame.TeamColor.WHITE;
+        return switch (piece.getPieceType()) {
+            case BISHOP -> isWhite ? WHITE_BISHOP : BLACK_BISHOP;
+            case PAWN -> isWhite ? WHITE_PAWN : BLACK_PAWN;
+            case KING -> isWhite ? WHITE_KING : BLACK_KING;
+            case QUEEN -> isWhite ? WHITE_QUEEN : BLACK_QUEEN;
+            case ROOK -> isWhite ? WHITE_ROOK : BLACK_ROOK;
+            case KNIGHT -> isWhite ? WHITE_KNIGHT : BLACK_KNIGHT;
+        };
+
     }
 }
